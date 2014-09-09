c5d8809b7c37d047472ecbcde9a7270d98116b06
Raghu Betina <rbetina@github.com>
2014-09-08 19:31:09 -0500

    Added associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
+
+  has_many :project_photos, :dependent => :destroy
+  has_many :projects, :through => :project_photos
+
+  has_many :taggings, :dependent => :destroy
+  has_many :tags, :through => :taggings
 end
