f7d88ee6a0b0e6e12b3b07dad2cddce6374d9b27
Raghu Betina <rbetina@github.com>
2014-09-08 20:03:44 -0500

    Scoping projects and photos index down to current user

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,7 @@ class PhotosController < ApplicationController
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.all
+    @photos = current_user.photos
   end
 
   # GET /photos/1
