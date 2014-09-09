9a05b4939d5ef8702cda527f98f3cf26c7c6f253
Raghu Betina <rbetina@github.com>
2014-09-08 20:00:24 -0500

    Removed user_id from permitted parameters

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :note, :user_id)
+      params.require(:project).permit(:title, :note)
     end
 end
