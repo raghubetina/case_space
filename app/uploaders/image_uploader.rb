31a9f31901c3c7d346558940cee356e27336f5a0
Raghu Betina <rbetina@github.com>
2014-09-09 16:54:05 -0500

    Slightly larger thumbnails

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [240, 240]
+    process :resize_to_fill => [300, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
