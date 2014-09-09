52bef20853082917ceca2941577ad6422fac392e
Raghu Betina <rbetina@github.com>
2014-09-08 19:43:14 -0500

    Assigning ownership of projects and photos in controller to current user

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -25,6 +25,7 @@ class PhotosController < ApplicationController
   # POST /photos.json
   def create
     @photo = Photo.new(photo_params)
+    @photo.user = current_user
 
     respond_to do |format|
       if @photo.save
